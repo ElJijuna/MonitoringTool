89f22cc49422c59d6aeb81447c8b2e8d9d817919
el_jijuna <pilmee@icloud.com>
2025-10-06 15:34:10 -0500

    docs: update docs

--- mock/scans/web-audit/report.mock.js
+++ mock/scans/web-audit/report.mock.js
@@ -3,16 +3,16 @@ import monitoringReport from './data/monitoring-tool';
 import reactBaseAppReport from './data/react-base-app';
 
 export default defineMock({
-  url: '/api/applications/:application/scans/web-audit',
+  url: '/api/web-audit-reports/:application',
   method: 'GET',
-  body({ params: { application } }) {
-    if (application === 'monitoring-tool') {
+  body({ params: { application = '' } }) {
+    if (application === 'monitoring-tool.json') {
       const webAuditReport = monitoringReport;
       return ({
         status: 'success',
         data: webAuditReport.value,
       });
-    } else if (application === 'react-base-app') {
+    } else if (application === 'react-base-app.json') {
       const webAuditReport = reactBaseAppReport;
       return ({
         status: 'success',
