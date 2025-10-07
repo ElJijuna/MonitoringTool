26de56c079ba140e3d02e9b92b588879fb54c99e
el_jijuna <pilmee@icloud.com>
2025-10-07 02:15:26 -0500

    feat: update components with mui charts.

--- src/components/WebAudit/WebAuditCWETypes/WebAuditCWETypes.tsx
+++ src/components/WebAudit/WebAuditCWETypes/WebAuditCWETypes.tsx
@@ -1,6 +1,17 @@
-import { type FC, type ReactElement } from 'react';
+import { useMemo, type FC, type ReactElement } from 'react';
 import { useWebAuditCWE } from '../hooks/useWebAuditCWE';
 import { theme } from 'antd';
+import { BarChart } from '@mui/x-charts/BarChart';
+
+const generateRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
 
 export interface WebAuditCWETypesProps {
   application: string;
@@ -11,13 +22,63 @@ export const WebAuditCWETypes: FC<WebAuditCWETypesProps> = ({ application }: Web
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
   const [data] = useWebAuditCWE({ application });
+  const values = useMemo<number[]>(() => Object.values(data.map(({ total }) => total)), [data]);
+  const categories = useMemo(() => data.map(({ code }) => code), [data]);
+  const randomBarColors = useMemo(() => categories.map(() => generateRandomColor()), [categories]);
 
-  return (<div
-    style={{
-      padding: 10,
-      minHeight: 360,
-      background: colorBgContainer,
-      borderRadius: borderRadiusLG,
-    }}
-  >{JSON.stringify(data)}</div>);
-}
+  console.log({values})
+  return (
+    <div
+      style={{
+        padding: 10,
+        background: colorBgContainer,
+        borderRadius: borderRadiusLG,
+      }}
+    >
+      <BarChart
+        hideLegend
+        borderRadius={4}
+        height={200}
+        series={[
+          {
+            data: values,
+            label: 'CWE',
+            type: 'bar',
+          },
+        ]}
+        yAxis={[{
+          offset: 4,
+          max: Math.max(...values),
+          domainLimit: 'strict',
+        }]}
+        xAxis={[{
+          scaleType: 'band',
+          data: categories,
+          colorMap: {
+            type: 'ordinal',
+            values: categories,
+            colors: randomBarColors,
+          },
+          tickLabelInterval: () => true,
+          valueFormatter: (value) => value,
+          tickLabelStyle: {
+            angle: -90,
+            textAnchor: 'end',
+            fontSize: 8,
+            textOverflow: 'clip',
+          },
+          labelStyle: {
+            textOverflow: 'clip',
+          },
+          offset: 4,
+        }]}
+        margin={{ bottom: 4 }}
+        sx={{
+          '.MuiChartsAxis-root .MuiChartsAxis-line': {
+            stroke: '#ccc',
+          },
+        }}
+      />
+    </div>
+  );
+};
--- src/components/WebAudit/WebAuditPie/WebAuditPie.tsx
+++ src/components/WebAudit/WebAuditPie/WebAuditPie.tsx
@@ -1,9 +1,9 @@
-import { Pie } from '@ant-design/charts';
+import { PieChart } from '@mui/x-charts/PieChart';
 import { useMemo, type FC, type PropsWithChildren, type ReactElement } from 'react';
 import { useWebAuditReport } from '../../../proxy-queries/useWebAuditReport';
 import { severityText } from '../../../utils/severity/severity-text';
-import { severityColor } from '../../../utils/severity/severity-color';
 import { theme } from 'antd';
+import { severityColor } from '../../../utils/severity/severity-color';
 
 export interface WebAuditPieProps extends PropsWithChildren {
   application: string;
@@ -14,52 +14,7 @@ export const WebAuditPie: FC<WebAuditPieProps> = ({ application }): ReactElement
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
   const { data: report, isPending } = useWebAuditReport({ application });
-  const data = useMemo(() => [{ type: severityText.critical, value: report?.metadata.vulnerabilities.critical ?? 0 }, { type: severityText.high, value: report?.metadata.vulnerabilities.high ?? 0 }, { type: severityText.moderate, value: report?.metadata.vulnerabilities.moderate ?? 0 }, { type: severityText.low, value: report?.metadata.vulnerabilities.low ?? 0 }, { type: severityText.info, value: report?.metadata.vulnerabilities.info ?? 0 }], [report]);
-  const config = useMemo(() => ({
-    data,
-    angleField: 'value',
-    colorField: 'type',
-    innerRadius: 0.6,
-    label: {
-      text: 'value',
-      style: {
-        fontWeight: 'bold',
-      },
-    },
-    legend: {
-      color: {
-        title: false,
-        rowPadding: 5,
-        align: 'center',
-      },
-    },
-    annotations: [
-      {
-        type: 'text',
-        style: {
-          text: 'Vulnerabilities',
-          x: '50%',
-          y: '50%',
-          textAlign: 'center',
-          fontSize: 16,
-          fontStyle: 'bold',
-        },
-      },
-    ],
-    scale: {
-      color: {
-        range: [
-          severityColor.critical,
-          severityColor.high,
-          severityColor.moderate,
-          severityColor.low,
-          severityColor.info
-        ],
-      },
-    },
-    responsive: true,
-    height: 300,
-  }), [data]);
+  const data = useMemo(() => [{ label: severityText.critical, value: report?.metadata.vulnerabilities.critical ?? 0, color: severityColor.critical }, { label: severityText.high, value: report?.metadata.vulnerabilities.high ?? 0, color: severityColor.high }, { label: severityText.moderate, value: report?.metadata.vulnerabilities.moderate ?? 0, color: severityColor.moderate }, { label: severityText.low, value: report?.metadata.vulnerabilities.low ?? 0, color: severityColor.low }, { label: severityText.info, value: report?.metadata.vulnerabilities.info ?? 0, color: severityColor.info }], [report]);
 
   if (isPending) {
     return <div>Loading...</div>;
@@ -69,10 +24,21 @@ export const WebAuditPie: FC<WebAuditPieProps> = ({ application }): ReactElement
     <div
       style={{
         padding: 10,
-        minHeight: 360,
         background: colorBgContainer,
         borderRadius: borderRadiusLG,
       }}
-    ><Pie {...config} /></div>
+    >
+      <PieChart
+        height={200}
+        series={[
+          {
+            paddingAngle: 5,
+            innerRadius: '60%',
+            outerRadius: '90%',
+            data,
+          },
+        ]}
+      />
+    </div>
   );
 };
